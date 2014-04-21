86a2be6ec1bee3687ed431938c6c84a535466e00
Keith Hamm <hkhamm@gmail.com>
2014-04-21 09:49:36 -0700

    cleanup comments

--- src/PrintBook.java
+++ src/PrintBook.java
@@ -30,12 +30,7 @@ public class PrintBook implements Printable {
             textTemp.add("");
         }
 
-        //System.out.println(textTemp);
         textLines = textTemp.toArray(new String[textTemp.size()]);
-
-//        for (String line : textLines) {
-//            System.out.println(line);
-//        }
     }
 
     @Override
@@ -81,48 +76,4 @@ public class PrintBook implements Printable {
         // of the printed document
         return PAGE_EXISTS;
     }
-
-//    public int print(Graphics g, PageFormat pf, int pageIndex)
-//            throws PrinterException {
-//
-//        Font font = new Font("Serif", Font.PLAIN, 10);
-//        FontMetrics metrics = g.getFontMetrics(font);
-//        int lineHeight = metrics.getHeight();
-//
-//        if (pageBreaks == null) {
-//            initTextLines();
-//            int linesPerPage = (int)(pf.getImageableHeight()/lineHeight);
-//            int numBreaks = (textLines.length-1)/linesPerPage;
-//            pageBreaks = new int[numBreaks];
-//            for (int b=0; b<numBreaks; b++) {
-//                pageBreaks[b] = (b+1)*linesPerPage;
-//            }
-//        }
-//
-//        if (pageIndex > pageBreaks.length) {
-//            return NO_SUCH_PAGE;
-//        }
-//
-//        /* User (0,0) is typically outside the imageable area, so we must
-//         * translate by the X and Y values in the PageFormat to avoid clipping
-//         * Since we are drawing text we
-//         */
-//        Graphics2D g2d = (Graphics2D)g;
-//        g2d.translate(pf.getImageableX(), pf.getImageableY());
-//
-//        /* Draw each line that is on this page.
-//         * Increment 'y' position by lineHeight for each line.
-//         */
-//        int y = 0;
-//        int start = (pageIndex == 0) ? 0 : pageBreaks[pageIndex-1];
-//        int end   = (pageIndex == pageBreaks.length)
-//                ? textLines.length : pageBreaks[pageIndex];
-//        for (int line=start; line<end; line++) {
-//            y += lineHeight;
-//            g.drawString(textLines[line], 0, y);
-//        }
-//
-//        /* tell the caller that this page is part of the printed document */
-//        return PAGE_EXISTS;
-//    }
 }
